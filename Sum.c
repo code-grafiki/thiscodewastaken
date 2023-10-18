d613e4fd9ca5bd5e8f418947e96c229321e53ba1
code-grafiki <80459355+code-grafiki@users.noreply.github.com>
2023-10-18 22:02:30 +0530

    Sum.c
    This is a c code for sum of 2 numbers

---
+++ Sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, sum;
+
+    // Input
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    // Sum the numbers
+    
+
+    // Output
+    printf("The sum of %.2lf and %.2lf is %.2lf\n", num1, num2, sum);
+
+    return 0;
+}
